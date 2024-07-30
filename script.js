d422e4c43c25f1a6dac3ac756037a16f553ed865
felipe-sbm <felipesbm2005@gmail.com>
2024-07-30 14:43:35 -0300

    Remoção do código por completo.

--- script.js
+++
@@ -1,9 +0,0 @@
- /* Função de subir para o topo da página */
-
-function top() {
-document.getElementById("scrollToTop").addEventListener("click", function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-}
-
- /* Função de  */
